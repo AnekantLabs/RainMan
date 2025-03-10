6959cbdaf83e039d5f5aad990b2274b32976e79d
Susmit Bahadkar <121481904+bobbybacala@users.noreply.github.com>
2025-03-10 11:08:18 +0530

    fast api setup w mongo, created a cluster on my personal mongo atlas (#3)

---
+++ app-backend/app/config.py
@@ -0,0 +1,9 @@
+# app/config.py
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
+DATABASE_NAME = os.getenv("DATABASE_NAME", "fastapi_db")
---
+++ app-backend/app/db.py
@@ -0,0 +1,21 @@
+# app/database.py
+from motor.motor_asyncio import AsyncIOMotorClient
+from app.config import MONGO_URI, DATABASE_NAME
+
+client = None
+db = None
+
+async def connect_to_mongo():
+    global client, db
+    try:
+        client = AsyncIOMotorClient(MONGO_URI)
+        db = client[DATABASE_NAME]
+        print("✅ Connected to MongoDB")
+    except Exception as e:
+        print(f"❌ Error connecting to MongoDB: {e}")
+
+async def close_mongo_connection():
+    global client
+    if client:
+        client.close()
+        print("❌ MongoDB connection closed")
